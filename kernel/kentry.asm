498fa20bb995f5bcda2b98e14ed6b8bb7743d9a9
AnggitUsdianto <gaito27otnaidsu@gmail.com>
2026-06-16 18:19:46 +0700

    feat:update kentry.asm to clear screen and put cursor at top-left of screen (0, 0), and update build script of makefile due to renamed source file name 'kernel.asm' to 'kentry.asm'

--- kernel/kentry.asm
+++ kernel/kentry.asm
@@ -1,13 +1,34 @@
 [BITS 32]
+[ORG 0x100000]
 
-global_start:
-_start:
-	mov edx, 0xB8000
-	mov al, 'X'
-	mov ah, 0x0F
-	mov [edx], ax
+kentry_start:
+	mov edi, 0xB8000
+	mov ecx, 2000
+	mov ax, 0x0F20
 
-hang:
-	jmp hang
+.clear_loop:
+	mov [edi], ax
+	add edi, 2
+	loop .clear_loop
+
+	mov al, 0x0E
+	mov dx, 0x3D4
+	out dx, al
+
+	mov al, 0x00
+	mov dx, 0x3D5
+	out dx, al
+
+	mov al, 0x0F
+	mov dx, 0x3D4
+	out dx, al
+
+	mov al, 0x00
+	mov dx, 0x3D5
+	out dx, al
+
+	jmp 0x100200
+
+	hlt
 
 times 512 - ($ - $$) db 0
